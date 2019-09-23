3bfc30e8090a9ee0e6bef490ad779f9d6a689a06
Martin Retamales <martin-retamales@mdscia.cl>
2019-09-22 21:34:45 -0300

    v1.1-riego-sensorhumedad-lcd

--- Riego.ino
+++ Riego.ino
@@ -1,13 +1,24 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+//Variables
 int pinRele = 9;
 int pinLedOff = 10;
 int pinLedOn = 11;
 int pinSensorHumedad = A0;
-
+char bufferValue[16];
+char bufferStatus[16];
 void setup() {
+  Wire.begin(); //Begin de port I2C
   Serial.begin(9600);
   pinMode(pinRele, OUTPUT);
   pinMode(pinLedOff, OUTPUT);
   pinMode(pinLedOn, OUTPUT);
+
+  lcd.clear();
+  lcd.init();
+  lcd.backlight();
 }
 
 // Rango de valores proporcionados por el sensor
@@ -20,13 +31,12 @@ int NivelDeHumedad(int humedad){
   int nivel;
   if(humedad > -1 && humedad < 600){
     nivel = 0;
-    }else{
+  }else{
         if(humedad > 599 && humedad < 701){
             nivel = 1;
           }
           else{
               if(humedad > 700 && humedad < 1024){
-                //message = message + "bajo";
                 nivel = 2;
                 }
             }
@@ -36,11 +46,20 @@ int NivelDeHumedad(int humedad){
 }
 
 void loop() {
+   //lcd.clear();
    int humedad = analogRead(pinSensorHumedad); //Read the parameter sensor humidity
    Serial.print("Sensor value monitored: "); //That line can be checked in serial monitor
    Serial.println(humedad); // Sensor value humidity
+   //lcd.print("Valor: "+humedad);
+   sprintf(bufferValue, "Ssr value:  %.4d",humedad);
+   lcd.setCursor(0,0);
+   lcd.print(bufferValue);
    delay(2000); //1 second equal to 1000 milliseconds
    int nivel = NivelDeHumedad(humedad); // Humidity Indicator
+   
+   sprintf(bufferStatus, "Ssr Status: %d",nivel);
+   lcd.setCursor(0,1);
+   lcd.print(bufferStatus);
    delay(2000); //1 second equal to 1000 milliseconds
     switch(nivel){
       case 0:analogWrite(pinLedOff, 255); // Always On when the level humidity is alto
